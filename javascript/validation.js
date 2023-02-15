ae4b289a8dc4d63a1ddfcc36686e7776016cc605
khadijasalamatasalou <saloujaba@gmail.com>
2023-02-15 11:58:37 +0000

    validation_formulaire

---
+++ javascript/validation.js
@@ -0,0 +1,24 @@
+  function emailValidation() {
+    const form = document.getElementById('form');
+    const emailComfirmField = document.getElementById('email_confirm');
+    const email = document.getElementById('email');
+    const element = document.createElement('p');
+    const message = document.createTextNode("E-mail incorrect");
+    const contentField = document.getElementById('content_field');
+    form.addEventListener('input', e => {
+        if(form.email.value !== form.email_confirm.value) {
+            contentField.parentNode.insertBefore(element, contentField);
+            element.appendChild(message);
+            element.classList.add("alert_color");
+            emailComfirmField.classList.add("alert_bg");
+      } else {
+        emailComfirmField.classList.remove("alert_bg");
+        element.parentNode.removeChild(element);
+      }
+    });
+  };
+  
+  window.onload = function() {
+    emailValidation();
+  };
+  
